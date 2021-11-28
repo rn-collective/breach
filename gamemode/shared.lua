3c0cf256dc15b6fe4055051ca6733f82044c92bc
Bilwin <bilwin780@gmail.com>
2021-11-28 14:50:28 +0900

    :arrow_up: Обновление режима

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -10,12 +10,22 @@ BREACH.Files = {
 	'meta/cl_player.lua',
 	'meta/sv_player.lua'
 }
+BREACH.Modules = {}
 
 function BREACH:Core()
 	for i = 1, #self.Files do
 		rnlib.i(self.Files[i])
 		rnlib.p('Loaded \'%s\'', self.Files[i])
 	end
+
+	-- modules
+	for dir, status in pairs(BREACH.Modules) do
+        if !status then continue end
+        for _, file in ipairs(file.Find('breach/gamemode/modules/'..dir..'/*.lua','LUA')) do
+            rnlib.i(dir..'/'..file)
+            rnlib.p('%s | Loaded \'%s\' module', dir, string.Right(file, #file - 3))
+        end
+    end
 end
 BREACH:Core()
 
@@ -26,9 +36,11 @@ end
 hook.Add('InitPostEntity', 'BREACH.Initialization', function()
 	RunConsoleCommand('sv_location', 'ru')
 	RunConsoleCommand('hostname', 'SCP: Snow Seazon > '..BREACH['Version'])
+
 	if SERVER then
 		BREACH:InitializeServer()
 	end
+
 	if CLIENT then
 		BREACH:InitializeClient()
 	end
