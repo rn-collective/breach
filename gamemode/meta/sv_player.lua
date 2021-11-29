d5d28da91fc0fc7912e345ce90a9dd87a5cbb88b
Bilwin <bilwin780@gmail.com>
2021-11-29 22:09:08 +0900

    :wrench: Обновление

--- gamemode/meta/sv_player.lua
+++ gamemode/meta/sv_player.lua
@@ -32,34 +32,3 @@ function PLAYER:DisableSpectator()
     self:SetNoTarget(false)
     self:GodDisable()
 end
-
-function PLAYER:SpawnByHelicopter(pos)
-    local pb = 8
-
-    local tr = util.TraceHull({
-        start = pos + Vector(0, 0, 72),
-        endpos = pos + Vector(0, 0, 2),
-        filter = {k, self},
-        maxs = Vector(pb, pb, 1),
-        mins = Vector(-pb, pb, 0)
-    })
-
-    local tr2 = util.TraceHull({
-        start = pos + Vector(0, 0, 2),
-        endpos = pos + Vector(0, 0, 72),
-        filter = {k, self},
-        maxs = Vector(pb, pb, 1),
-        mins = Vector(-pb, pb, 0)
-    })
-
-    if !tr.Hit && !tr.StartSolid && !tr2.Hit && !tr2.StartSolid then
-        self:SetPos(pos)
-        self:SetEyeAngles(Angle(0, math.Rand(-180, 180), 0))
-
-        local insertion = ents.Create 'mw_infil'
-        insertion:SetPos(pos)
-        insertion:SetAngles(Angle(0, math.Rand(-180, 180), 0))
-        insertion:SetOwner(self)
-        insertion:Spawn()
-    end
-end
